47c879034b04b32d0a10eb99c46acfc25a086ca0
Raylander <raylander.guimaraes@gmail.com>
2022-11-22 16:01:08 -0300

    adiciona botão github projetos

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnExpand = document.getElementById("btn-expand")
+
+let menuExpanded = document.getElementById("menu-epanded")
+
+function mostraMenu() {
+   
+}
+
+btnExpand.addEventListener('click', mostraMenu )
